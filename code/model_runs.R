8a144e13db05ca322950ea70e742855be40c9363
Brian Langseth <brian.langseth@noaa.gov>
2025-03-03 16:26:20 -0800

    Update some model runs

--- code/model_runs.R
+++ code/model_runs.R
@@ -95,6 +95,8 @@ dev.off()
 ## 0_0_1_2021base  ----
 ####------------------------------------------------#
 
+# Use most up to date SS3 executable with the 2021 base model
+
 new_name <- "0_0_1_2021base"
 
 ##
@@ -145,6 +147,100 @@ SSsummarize(xx) |>
                     subplots = c(1,3), print = TRUE, plotdir = here('models', new_name))
 
 dev.off()
+
+
+
+####------------------------------------------------#
+# 0_0_2_2025setup  ----
+####------------------------------------------------#
+
+# Change file names and comments to reflect a 2025 model
+
+new_name <- "0_0_2_2025setup"
+old_name <- "0_0_1_2021base"
+
+##
+#Copy inputs
+##
+
+#Use ss_new inputs so have commonality with other SS3 models
+copy_SS_inputs(dir.old = here('models', old_name), 
+               dir.new = here('models', new_name),
+               use_ss_new = TRUE,
+               overwrite = TRUE)
+
+mod <- SS_read(here('models', new_name))
+
+
+##
+#Make Changes
+##
+
+## Starter file
+
+#Change name for files
+mod$start$datfile <- "2025_ca_quillback.dat"
+mod$start$ctlfile <- "2025_ca_quillback.ctl"
+
+
+
+
+#Change comments in data file
+mod$dat$Comments[1] <- "#California Quillback Rockfish 2025 assessment Brian Langseth, Melissa Monk, Julia Coates"
+
+## Forecast file
+
+#Reset benchmark years which were previously hard code. -999 is first model year, 0 is last 
+mod$fore$Bmark_years <- c(-999,0, 0,0, 0,0, -999,0, -999,0) #start year and end year for all but selectivity (because of blocks) and relF
+mod$fore$Nforecastyrs <- 12
+mod$fore$Fcast_years <- c(0,0, -3,0, -999,0) #last year for selex, last three years for relF, full time series for average recruitment (though using fcast_rec_option = 0 ignores this)
+
+
+
+
+
+
+##
+#Output files and run
+##
+
+#Rename files for 2025
+file.rename(from = here('models', new_name, "2021_ca_quillback.dat"),
+            to = here('models', new_name, "2025_ca_quillback.dat"))
+file.rename(from = here('models', new_name, "2021_ca_quillback.ctl"),
+            to = here('models', new_name, "2025_ca_quillback.ctl"))
+
+
+SS_write(mod,
+         dir = here('models', new_name),
+         overwrite = TRUE)
+
+r4ss::run(dir = here('models', new_name), 
+          exe = here('models/ss3_win.exe'), 
+          extras = '-nohess',
+          show_in_console = TRUE, #comment out if you dont want to watch model iterations
+          skipfinished = FALSE)
+
+pp <- SS_output(here('models', new_name))
+SS_plots(pp, plot = c(1:26))
+
+
+##
+#Comparison plots
+##
+
+xx <- SSgetoutput(dirvec = glue::glue("{models}/{subdir}", models = here('models'),
+                                      subdir = c('name1',
+                                                 'name2',
+                                                 new_name)))
+SSsummarize(xx) |>
+  SSplotComparisons(legendlabels = c('label for model 1',
+                                     'label for model 2',
+                                     'label for new model'),
+                    subplots = c(1,3), print = TRUE, plotdir = here('models', new_name))
+
+dev.off()
+
 
 
 ##########################################################################################-
