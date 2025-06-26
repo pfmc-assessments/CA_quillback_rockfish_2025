37cf4976eecf66527a614d43b16efd8125553e86
Melissa Monk <melissa.monk@noaa.gov>
2025-06-26 15:03:16 -0700

    Request13 remove growth fleet

--- code/model_runs_sensitivities.R
+++ code/model_runs_sensitivities.R
@@ -4653,3 +4653,72 @@ plot_sel_all(pp)
 
 pp$sigma_R_info #0.82
 
+######-
+## Request  13 remove the growth fleet completely
+##
+new_name <- 'STAR_Request13_nogrowthfleet'
+old_name <- base_mod_name
+
+copy_SS_inputs(dir.old = here('models', old_name), 
+               dir.new = here(sens_dir, new_name),
+               overwrite = TRUE)
+
+mod <- SS_read(here(sens_dir,new_name))
+
+#Remove growth fleet CAAL data 
+mod$dat$agecomp[mod$dat$agecomp$fleet == 3, "year"] <- -abs(mod$dat$agecomp[mod$dat$agecomp$fleet == 3, "year"])
+
+
+# Write model and run
+SS_write(mod, here(sens_dir, new_name),
+         overwrite = TRUE)
+
+r4ss::run(dir = here(sens_dir, new_name), 
+          exe = here('models/ss3_win.exe'), 
+          extras = '-nohess', 
+          show_in_console = TRUE, 
+          skipfinished = FALSE)
+
+pp <- SS_output(here(sens_dir, new_name))
+SS_plots(pp, plot = c(1:26))
+plot_sel_all(pp)
+
+######-
+## Request 13 remove the growth fleet completely - reweighting
+
+new_name <- "STAR_request13_nogrowthfleet_reweight"
+
+mod <- SS_read(here('models', '_sensitivities', "STAR_request13_nogrowthfleet"))
+
+#Run based on weights set to one
+mod$ctl$Variance_adjustment_list$value <-  1
+
+SS_write(mod,
+         dir = here('models', '_sensitivities', new_name),
+         overwrite = TRUE)
+
+r4ss::run(dir = here('models', '_sensitivities', new_name), 
+          exe = here('models/ss3_win.exe'), 
+          extras = '-nohess',
+          show_in_console = TRUE,
+          skipfinished = FALSE)
+
+#Now iteratively reweight based on weight = 1 run and reassign weights
+pp <- SS_output(here('models', '_sensitivities', new_name))
+iter <- 3
+dw <- r4ss::tune_comps(replist = pp, 
+                       option = 'Francis', 
+                       dir = here('models', '_sensitivities', new_name), 
+                       exe = here('models/ss3_win.exe'), 
+                       niters_tuning = iter, 
+                       #extras = '-nohess',
+                       allow_up_tuning = TRUE,
+                       show_in_console = TRUE)
+
+pp <- SS_output(here('models', '_sensitivities', new_name))
+SS_plots(pp, plot = c(1:26))
+plot_sel_all(pp)
+
+alt_sigmaR <- pp$sigma_R_info[pp$sigma_R_info$period == "Main", "alternative_sigma_R"]
+pp$sigma_R_info
+
--- code/temp_model_runs.r
+++ code/temp_model_runs.r
@@ -15,41 +15,21 @@ sens_dir <- here('models', '_sensitivities')
 base_mod_name <- '5_1_3_preStarBase' #<---------------UPDATE WHEN CHANGE
 base_mod <- SS_read(here('models', base_mod_name))
 
-
-###fix growth to internal and leave out ages
-new_name <- 'fix_growth_external_no_ages'
+######-
+## Request  13 remove the growth fleet completely
+##
+new_name <- 'STAR_Request13_nogrowthfleet'
 old_name <- base_mod_name
 
 copy_SS_inputs(dir.old = here('models', old_name), 
-               dir.new = here('models', new_name),
+               dir.new = here(sens_dir, new_name),
                overwrite = TRUE)
 
-mod <- SS_read(here('models',new_name))
+mod <- SS_read(here(sens_dir,new_name))
 
- #         K        Linf          L0         CV0         CV1
- #0.17840051 41.17135518  3.99192977  0.20157461  0.06413399
-#This is L0 so change that in the model
+#Remove growth fleet CAAL data 
+mod$dat$agecomp[mod$dat$agecomp$fleet == 3, "year"] <- -abs(mod$dat$agecomp[mod$dat$agecomp$fleet == 3, "year"])
 
-mod$ctl$MG_parms$INIT[2] <-  3.99192977
-mod$ctl$MG_parms$INIT[3] <- 41.17135518	
-mod$ctl$MG_parms$INIT[4] <-  0.17840051	
-mod$ctl$MG_parms$INIT[5] <-  0.20157461
-mod$ctl$MG_parms$INIT[6] <-  0.06413399
-mod$ctl$MG_parms$PRIOR[2:6] <- mod$ctl$MG_parms$INIT[2:6]
-#negative phase 
-mod$ctl$MG_parms$PHASE[2:6] <- -9
-
-
-# Create a lambda section 
-lambdas <- data.frame("like_comp" = c(5, 5), #age comps
-                      "fleet" = c(1, 3),
-                      "phase" = c(1, 1),
-                      "value" = c(0, 0),
-                      "sizefreq_method" = c(1, 1))
-rownames(lambdas) <- c("CAAL_CA_Commercial", "CAAL_CA_Growth")
-
-mod$ctl$N_lambdas <- nrow(lambdas)
-mod$ctl$lambdas <- lambdas
 
 # Write model and run
 SS_write(mod, here(sens_dir, new_name),
@@ -65,53 +45,55 @@ pp <- SS_output(here(sens_dir, new_name))
 SS_plots(pp, plot = c(1:26))
 plot_sel_all(pp)
 
+######-
+## Request 13 remove the growth fleet completely - reweighting
+
+new_name <- "STAR_request13_nogrowthfleet_reweight"
+
+mod <- SS_read(here('models', '_sensitivities', "STAR_request13_nogrowthfleet"))
+
+#Run based on weights set to one
+mod$ctl$Variance_adjustment_list$value <-  1
+
+SS_write(mod,
+         dir = here('models', '_sensitivities', new_name),
+         overwrite = TRUE)
+
+r4ss::run(dir = here('models', '_sensitivities', new_name), 
+          exe = here('models/ss3_win.exe'), 
+          extras = '-nohess',
+          show_in_console = TRUE,
+          skipfinished = FALSE)
+
+#Now iteratively reweight based on weight = 1 run and reassign weights
+pp <- SS_output(here('models', '_sensitivities', new_name))
+iter <- 3
+dw <- r4ss::tune_comps(replist = pp, 
+                       option = 'Francis', 
+                       dir = here('models', '_sensitivities', new_name), 
+                       exe = here('models/ss3_win.exe'), 
+                       niters_tuning = iter, 
+                       #extras = '-nohess',
+                       allow_up_tuning = TRUE,
+                       show_in_console = TRUE)
+
+pp <- SS_output(here('models', '_sensitivities', new_name))
+SS_plots(pp, plot = c(1:26))
+plot_sel_all(pp)
+
+alt_sigmaR <- pp$sigma_R_info[pp$sigma_R_info$period == "Main", "alternative_sigma_R"]
+pp$sigma_R_info
+
+
+
+
+
+
+
+
+
+
 xx <- SSgetoutput(dirvec = c(glue::glue("{models}/{subdir}", models = here('models'),
                                         subdir = c(base_mod_name,
                                                    file.path('_sensitivities', new_name)))))
 
-
-
-
-##
-#Make Changes
-##
-
-mod$ctl$blocks_per_pattern <- c(3, 2)
-mod$ctl$Block_Design <- list(c(2003, 2013, 2014, 2022, 2023, 2024), #commercial fleet
-                             c(2001, 2022, 2023, 2024)) #recreational fleet
-
-
-
-### Add block indicators into selectivity table
-# Block = number of block to use, Block_Fxn = 2 means replace parameters 
-selex_new <- mod$ctl$size_selex_parms
-
-selex_new[intersect(grep("Commercial", rownames(selex_new)), which(selex_new$PHASE > 0)), 
-          c("Block")] <- 1
-selex_new[intersect(grep("Commercial", rownames(selex_new)), which(selex_new$PHASE > 0)), 
-          c("Block_Fxn")] <- 2
-selex_new[intersect(grep("Recreational", rownames(selex_new)), which(selex_new$PHASE > 0)), 
-          c("Block")] <- 2
-selex_new[intersect(grep("Recreational", rownames(selex_new)), which(selex_new$PHASE > 0)), 
-          c("Block_Fxn")] <- 2
-
-mod$ctl$size_selex_parms <- selex_new
-
-
-### Time varying selectivity table
-selex_tv_pars <- dplyr::filter(selex_new, Block > 0) |>
-  dplyr::select(LO, HI, INIT, PRIOR, PR_SD, PR_type, PHASE, Block) |>
-  tidyr::uncount(mod$ctl$blocks_per_pattern[Block], .id = 'id', .remove = FALSE)
-
-rownames(selex_tv_pars) <- rownames(selex_tv_pars) |>
-  stringr::str_remove('\\.\\.\\.[:digit:]+') |>
-  stringr::str_c('_BLK', selex_tv_pars$Block, 'repl_', mapply("[",mod$ctl$Block_Design[selex_tv_pars$Block], selex_tv_pars$id * 2 - 1))
-
-mod$ctl$size_selex_parms_tv <- selex_tv_pars |>
-  dplyr::select(-Block, -id)
-
-#estimate param 4 in recreational time blocks
-
-  mod$ctl$size_selex_parms_tv[intersect(grep("Recreational", rownames(mod$ctl$size_selex_parms_tv)),
-                                     grep("P_4", rownames(mod$ctl$size_selex_parms_tv))), 
-                           c("PHASE")] <- c(4)
