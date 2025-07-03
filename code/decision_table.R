63bb98b12960766b6944f213e5ecb4aa6077020b
Brian Langseth <brian.langseth@noaa.gov>
2025-07-03 13:43:11 -0700

    Add decision table runs and table

---
+++ code/decision_table.R
@@ -0,0 +1,120 @@
+library(r4ss)
+library(here)
+library(dplyr)
+
+source(here('code/table_decision_format.R'))
+
+base_mod_name <- '6_0_1_postStarBase' 
+base_mod <- SS_read(here('models', base_mod_name))
+
+pstar <- 0.45
+
+
+#####-
+## Low decision table M = 0.0525 ---------------------------------
+
+mod <- base_mod
+
+#Upate catches based on base model
+fore_catch <- r4ss::SS_ForeCatch(base_mod, yrs = 2025:2036)
+mod$fore$ForeCatch <- fore_catch[,c(1:4)]
+
+#Turn off buffers
+mod$fore$Flimitfraction <- 1 #dont have years of buffer applied
+mod$fore$FirstYear_for_caps_and_allocations <- 2037 #these should be overwritten with the fixed catch but putting here anyway
+
+#Change M value
+mod$ctl$MG_parms["NatM_p_1_Fem_GP_1", c("INIT", "PRIOR")] <- c(0.0525, round(log(0.0525),2))
+
+
+##
+#Output files and run
+##
+
+SS_write(mod,
+         dir = here('models', paste0(base_mod_name, "_low_M_pstar_",pstar)),
+         overwrite = TRUE)
+
+r4ss::run(dir = here('models', paste0(base_mod_name, "_low_M_pstar_",pstar)),
+          exe = here('models/ss3_win.exe'),
+          extras = '-nohess',
+          show_in_console = TRUE,
+          skipfinished = FALSE)
+
+
+#####-
+## High decision table M = 0.08 ---------------------------------
+
+mod <- base_mod
+
+#Upate catches based on base model
+fore_catch <- r4ss::SS_ForeCatch(base_mod, yrs = 2025:2036)
+mod$fore$ForeCatch <- fore_catch[,c(1:4)]
+
+#Turn off buffers
+mod$fore$Flimitfraction <- 1 #dont have years of buffer applied
+mod$fore$FirstYear_for_caps_and_allocations <- 2037 #these should be overwritten with the fixed catch but putting here anyway
+
+#Change M value
+mod$ctl$MG_parms["NatM_p_1_Fem_GP_1", c("INIT", "PRIOR")] <- c(0.08, round(log(0.08),2))
+
+
+##
+#Output files and run
+##
+
+SS_write(mod,
+         dir = here('models', paste0(base_mod_name, "_high_M_pstar_",pstar)),
+         overwrite = TRUE)
+
+r4ss::run(dir = here('models', paste0(base_mod_name, "_high_M_pstar_",pstar)),
+          exe = here('models/ss3_win.exe'),
+          extras = '-nohess',
+          show_in_console = TRUE,
+          skipfinished = FALSE)
+
+
+
+# #####-------------------------------------------####-
+# #Decision Table --------------------------------Moved into 002_load_tables and 01_executive summary
+# #####-------------------------------------------####-
+# 
+# low45 <- SS_output(here('models', paste0(base_mod_name, "_low_M_pstar_",pstar)))
+# base45 <- SS_output(here('models', base_mod_name))
+# high45 <- SS_output(here('models', paste0(base_mod_name, "_high_M_pstar_",pstar)))
+# 
+# # low40 <- SS_output(here('models','decision_tables',"low_0.4"))
+# # base40 <- SS_output(here('models','decision_tables',"base_0.4"))
+# # high40 <- SS_output(here('models','decision_tables',"high_0.4"))
+# 
+# caption <- "Decision table with 10-year projections beginning in 2027 for alternative states of nature based around
+# modeling natural mortality. 'Mgmt' refers to the two management scenarios (A) the default harvest control rule
+# $P^* = 0.45$, and (B) harvest control rule with a lower $P^* = 0.40$. Catch (in mt) is from the projections from the
+# base model for each management scenario, and is applied to each state of nature. Catches in 2025
+# and 2026 are fixed at the ACLs and have been set for that year with values provided by the GMT. The alternative
+# states of nature ('Low', 'Base', and 'High') are provided in the columns, and assume natural mortality is fixed either
+# a low value (Low M; low state), or a high value (High M; high state). Spawning output ('Spawn', in billions of eggs) and
+# fraction of unfished ('Frac') is provided for each state of nature."
+# 
+# tab <- table_decision(
+#   caption = caption,
+#   label = "es-decision",
+#   list(low45, base45, high45)#,
+#   #list(low40, base40, high40)
+# )
+# writeLines(tab,here('report', "tables", "decision_table_es.tex"))
+# 
+# # tab <- table_decision(
+# #   caption = caption,
+# #   label = "dec-tab",
+# #   list(low45, base45, high45),
+# #   list(low40, base40, high40)
+# # )
+# # writeLines(tab,here('report', "tables", "decision_table.tex"))
+
+
+
+
+
+
+
---
+++ models/6_0_1_postStarBase_low_M_pstar_0.45/forecast.ss
@@ -0,0 +1,78 @@
+#C file created using an r4ss function
+#C file write time: 2025-07-03  13:08:58
+#
+1 #_benchmarks
+2 #_MSY
+0.5 #_SPRtarget
+0.4 #_Btarget
+#_Bmark_years: beg_bio, end_bio, beg_selex, end_selex, beg_relF, end_relF,  beg_recr_dist, end_recr_dist, beg_SRparm, end_SRparm (enter actual year, or values of 0 or -integer to be rel. endyr)
+-999 0 0 0 0 0 -999 0 -999 0
+2 #_Bmark_relF_Basis
+1 #_Forecast
+12 #_Nforecastyrs
+1 #_F_scalar
+-12345  # code to invoke new format for expanded fcast year controls
+# biology and selectivity vectors are updated annually in the forecast according to timevary parameters, so check end year of blocks and dev vectors
+# input in this section directs creation of averages over historical years to override any time_vary changes
+#_Types implemented so far: 1=M, 4=recr_dist, 5=migration, 10=selectivity, 11=rel. F, recruitment
+#_list: type, method (1, 2), start year, end year
+#_Terminate with -9999 for type
+#_ year input can be actual year, or values <=0 to be rel. styr or endyr
+#_Method = 0 (or omitted) means continue using time_vary parms; 1 means to use average of derived factor
+ #_MG_type method st_year end_year
+        10      1       0        0
+        11      1      -3        0
+        12      1    -999        0
+-9999 0 0 0
+3 #_ControlRuleMethod
+0.4 #_BforconstantF
+0.1 #_BfornoF
+1 #_Flimitfraction
+3 #_N_forecast_loops
+3 #_First_forecast_loop_with_stochastic_recruitment
+0 #_fcast_rec_option
+1 #_fcast_rec_val
+0 #_Fcast_loop_control_5
+2037 #_FirstYear_for_caps_and_allocations
+0 #_stddev_of_log_catch_ratio
+0 #_Do_West_Coast_gfish_rebuilder_output
+1999 #_Ydecl
+1 #_Yinit
+1 #_fleet_relative_F
+# Note that fleet allocation is used directly as average F if Do_Forecast=4 
+2 #_basis_for_fcast_catch_tuning
+# enter list of fleet number and max for fleets with max annual catch; terminate with fleet=-9999
+-9999 -1
+# enter list of area ID and max annual catch; terminate with area=-9999
+-9999 -1
+# enter list of fleet number and allocation group assignment, if any; terminate with fleet=-9999
+-9999 -1
+2 #_InputBasis
+ #_#Year Seas Fleet dead(B)
+    2025    1     1    0.09
+    2025    1     2    1.21
+    2026    1     1    0.09
+    2026    1     2    1.41
+    2027    1     1    5.13
+    2027    1     2    6.87
+    2028    1     1    5.33
+    2028    1     2    7.03
+    2029    1     1    5.48
+    2029    1     2    7.12
+    2030    1     1    5.58
+    2030    1     2    7.15
+    2031    1     1    5.62
+    2031    1     2    7.13
+    2032    1     1    5.63
+    2032    1     2    7.07
+    2033    1     1    5.61
+    2033    1     2    7.00
+    2034    1     1    5.56
+    2034    1     2    6.91
+    2035    1     1    5.50
+    2035    1     2    6.83
+    2036    1     1    5.43
+    2036    1     2    6.74
+-9999 0 0 0
+#
+999 # verify end of input 
