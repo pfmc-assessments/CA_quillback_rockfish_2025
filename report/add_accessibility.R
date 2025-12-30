0d02188285eff65cfaac6e80689ead89568a7f8b
Brian Langseth <brian.langseth@noaa.gov>
2025-12-30 12:32:27 -0800

    Changes to reflect if use asar version 1.6.1
    Done Dec30. Note this version was not what was sent to the Council because the spacing was not as clean.

--- report/add_accessibility.R
+++ report/add_accessibility.R
@@ -1,4 +1,4 @@
 asar::add_accessibility(x = "SAR_C_Quillback_rockfish_skeleton.tex",
                         dir = here::here("report"),
                         alttext_csv = "report/captions_alt_text.csv",
-                        rename = "Quillback_rockfish_SAR_2025_Accessible")
+                        rename = "Quillback_rockfish_SAR_2025_Accessible_30Dec2025")
